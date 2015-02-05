1c022095428b38ced629020e92a5f231d15c2755
Zachary Kirchin <zacharykirchin@gmail.com>
2015-02-05 12:27:22 -0500

    fix carrierwave upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,10 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
