f037b31a81105b8299beca90188bd4da1b8dc5e9
Zachary Kirchin <zacharykirchin@gmail.com>
2015-02-06 13:35:22 -0500

    ftp_url

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,6 @@ CarrierWave.configure do |config|
   config.ftp_user = "tts-family"
   config.ftp_passwd = "hcn7o8gHfKMf!"
   config.ftp_folder = "/tts-family-uploads"
-  config.ftp_url = "http://techtalentsouth.com/tts-family-uploads"
-  config.ftp_passive = false # false by default
+  config.ftp_url = "ftp://techtalentsouth.com/tts-family-uploads"
+  config.ftp_passive = true # false by default
 end
