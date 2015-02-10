156c750465475b32f9ea01dd1eece88d2a9782be
Zachary Kirchin <zacharykirchin@gmail.com>
2015-02-10 09:25:14 -0500

    carrierwave ftp fix I think

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,7 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  # process :scale => [250, 250]
   #
   # def scale(width, height)
   #   # do something
@@ -40,9 +40,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.ftp_host = "techtalentsouth.com"
   config.ftp_port = 21
-  config.ftp_user = "tts-family"
+  config.ftp_user = "ttsdeveloper"
   config.ftp_passwd = "hcn7o8gHfKMf!"
   config.ftp_folder = "/tts-family-uploads"
-  config.ftp_url = "ftp://techtalentsouth.com/tts-family-uploads"
+  config.ftp_url = "http://techtalentsouth.com/tts-family-uploads"
   config.ftp_passive = true # false by default
 end
